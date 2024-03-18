48fe68db329d65e9f48f499c66120905bb048115
fadilikrm <rvddsr@gmail.com>
2024-03-18 21:58:44 +0800

    Menambahkan Opsi Keluar pada Games

--- games.py
+++ games.py
@@ -5,6 +5,7 @@ def main():
     print("1. Kertas")
     print("2. Batu")
     print("3. Gunting")
+    print("4. Keluar")
     
     pilih = int(input("Masukkan Pilihan Anda: "))
     comp = random.randint(1,3)
@@ -48,6 +49,9 @@ def main():
             print("Kamu Memilih Gunting")
             print("Komputer Memilih Gunting")
             print("Seri!")
+    elif pilih == 4:
+        print("Terima kasih")
+        exit()
     else:
         print("Pilihan Tidak Tersedia")
 
